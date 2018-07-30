6d552fb3ea61001b7a5ca3fadc189971b75ed148
Riya Abraham <riyamaryabraham12@gmail.com>
2018-07-30 00:20:17 -0500

    scrapping

--- server.js
+++ server.js
@@ -28,7 +28,14 @@ app.use(bodyParser.json()); // Send JSON responses
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+//mongoose.connect("mongodb://localhost/week18Populater");
 
 // Routes
 
